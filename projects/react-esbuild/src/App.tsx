51de28cc5cab8f2aa507193dc051929e9ba563e6
xuhongbin <xuhongbin@yy.com>
2021-04-16 02:58:07 +0800

    chore: up

--- projects/react-esbuild/src/App.tsx
+++ projects/react-esbuild/src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'https://esm.sh/react'
+import confetti from 'https://cdn.skypack.dev/canvas-confetti'
 import './app.scss'
 import d from './app.json'
 console.log(d)
+confetti()
+setInterval(() => confetti(), 5000)
 export default function App() {
-  return <div className="app">Hello ESMP!!</div>
+  return (
+    <div className="app">
+      <h1>Hello ESMP</h1>
+      <p>use esm instead module federation</p>
+    </div>
+  )
 }
