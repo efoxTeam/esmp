181d6e7bab67e2021773ef31097e0bb9ece9c0b4
xuhongbin <xuhongbin@yy.com>
2021-04-16 15:34:17 +0800

    chore: 优化构建脚本

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # ESMP
 emp on esm [demo](https://esmp.vercel.app/)
 
+# 启动 
++ `yarn` 安装依赖、建立连接 
++ `yarn compile` 构建依赖库  
+
 ## 支撑列表 
 + css module [about](https://github.com/evanw/esbuild/issues/20)
 + hmr `doing`
---
+++ packages/esmp/bin/esmp.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+const program = require('commander') // 命令行工具
+const pkg =require('../package.json')
+program.version(pkg.version, '-v, --version').usage('<command> [options]')
+program
+  .command('dev')
+  .description('调试项目')
+  .option('-s, --src <src>', '目标文件 默认为 src/index.ts')
+  .option('-pc, --public <public>', '目标 默认为 public/')
+  .option('-e, --env <env>', '部署环境 dev、test、prod 默认为 dev')
+  .option('-h, --hot', '是否使用热更新 默认不启动')
+  .action(d => {
+    require('../dist/scripts/dev')(d)
+  })
+
+//
+program
+  .command('build')
+  .description('构建项目')
+  .option('-s, --src <src>', '目标文件 默认为 src/index.ts')
+  .option('-d, --dist <dist>', '目标 默认为 dist/')
+  .option('-e, --env <env>', '部署环境 dev、test、prod 默认为 prod')
+  .action(d => {
+    require('../dist/scripts/build')(d)
+  })
+
+program.parse(process.argv)
